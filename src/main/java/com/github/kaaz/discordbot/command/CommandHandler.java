9478c0603a51a30e99c7765470b5cc8c97b28afa
nija123098 <nija123098@gmail.com>
2017-02-27 00:47:18 -0600

    Reduce config lookup time

--- src/main/java/com/github/kaaz/discordbot/command/CommandHandler.java
+++ src/main/java/com/github/kaaz/discordbot/command/CommandHandler.java
@@ -1,9 +1,12 @@
 package com.github.kaaz.discordbot.command;
 
 import com.github.kaaz.discordbot.util.Log;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
+import org.apache.commons.lang3.tuple.Triple;
 import org.reflections.Reflections;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,7 +16,7 @@ import java.util.Set;
  * Made by nija123098 on 2/20/2017.
  */
 public class CommandHandler {
-    private static final List<AbstractCommand> COMMANDS;
+    private static final Map<String, AbstractCommand>[] SORTED_COMMANDS;
     static {
         Reflections reflections = new Reflections("com.github.kaaz.discordbot.command.commands");
         Set<Class<? extends AbstractSuperCommand>> supers = reflections.getSubTypesOf(AbstractSuperCommand.class);
@@ -28,7 +31,6 @@ public class CommandHandler {
             }
             superCommands.add(superCommand);
         });
-        COMMANDS = new ArrayList<>(superCommands);
         Set<Class<? extends AbstractSubCommand>> subs = reflections.getSubTypesOf(AbstractSubCommand.class);
         Map<String, List<Class<? extends AbstractSubCommand>>> subMap = new HashMap<>(subs.size());
         subs.forEach(clazz -> {
@@ -38,6 +40,8 @@ public class CommandHandler {
             }
             subMap.get(pac).add(clazz);
         });
+        List<AbstractCommand> commands = new ArrayList<>(superCommands.size() + subs.size());
+        commands.addAll(superCommands);
         superCommands.forEach(superCommand -> {
             List<Class<? extends AbstractSubCommand>> classes = subMap.get(superCommand.getClass().getPackage().toString());
             if (classes != null){
@@ -49,10 +53,84 @@ public class CommandHandler {
                         Log.log("Exception while initing command: " + clazz.getSimpleName(), e);
                         return;
                     }
-                    COMMANDS.add(subCommand);
+                    commands.add(subCommand);
                     subCommand.setSuperCommand(superCommand);
                 });
             }
         });
+        Map<String, AbstractCommand>[] sortedCommands = new Map[7];
+        for (int i = 0; i < sortedCommands.length; i++) {
+            sortedCommands[i] = new HashMap<>();// no modifications will be made after this init
+        }
+        commands.forEach(abstractCommand -> abstractCommand.getAliases().forEach(ali -> {
+            String[] strings = ali.split(" ");
+            AbstractCommand repeat = sortedCommands[strings.length].get(ali);
+            if (repeat == null){
+                sortedCommands[strings.length].put(ali, abstractCommand);
+            } else {
+                Log.log("Duplicate alias found: " + abstractCommand.getClass().getName() + " " + repeat.getClass().getName() + " " + ali);
+            }
+        }));
+        int high = 0;
+        for (int i = sortedCommands.length - 1; i > 0; --i) {
+            if (!sortedCommands[i].isEmpty()){
+                high = i;
+                break;
+            }
+        }
+        SORTED_COMMANDS = new Map[high];
+        for (int i = 1; i < high; i++) {// i = 1, no command name ""
+            if (!sortedCommands[i].isEmpty()){
+                SORTED_COMMANDS[i] = sortedCommands[i];
+            }// could probably remake the map with the precise count of elements
+        }
+    }
+    public static Triple<Boolean, String, AbstractCommand> getCommand(String commandIn){
+        String commandReduced;
+        {
+            char[] original = commandIn.toCharArray();
+            char[] reducedChars = new char[original.length];
+            boolean hadSpace = true;
+            int index = commandIn.indexOf("  ") - 3;
+            if (index != -4){
+                for (int i = index; i < reducedChars.length; ++i) {
+                    if (original[i] == ' '){
+                        if (!hadSpace){
+                            hadSpace = true;
+                            reducedChars[++index] = original[i];
+                        }
+                    } else {
+                        reducedChars[++index] = original[i];
+                        hadSpace = false;
+                    }
+                }
+            }
+            commandReduced = new String(reducedChars).substring(1, index + 1);
+        }
+        String[] strings = commandReduced.split(" ");
+        String n;
+        if (strings.length > SORTED_COMMANDS.length){
+            strings = Arrays.copyOfRange(strings, 0, SORTED_COMMANDS.length);
+        }
+        AbstractCommand command;
+        for (int i = strings.length - 1; i > - 1; --i) {
+            n = "";
+            for (int j = strings.length - i - 1; j > - 1; --j) {
+                n += strings[j];
+            }
+            command = SORTED_COMMANDS[i].get(n);
+            if (command != null){
+                for (String string : strings) {
+                    commandIn = commandIn.substring(string.length());
+                    for (int j = 0; j < commandIn.length(); j++) {
+                        if (commandIn.charAt(j) != ' '){
+                            commandIn = commandIn.substring(j);
+                        }
+                    }
+                }
+                return new ImmutableTriple<>(command.getEmoticonAliases().contains(n), commandIn, command);
+            }
+        }
+        return null;
     }
 }
--- src/main/java/com/github/kaaz/discordbot/config/ConfigHandler.java
+++ src/main/java/com/github/kaaz/discordbot/config/ConfigHandler.java
@@ -4,18 +4,23 @@ import com.github.kaaz.discordbot.util.Holder;
 import com.github.kaaz.discordbot.util.Log;
 import org.reflections.Reflections;
 
-import java.util.ArrayList;
 import java.util.EnumSet;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 /**
  * Made by nija123098 on 2/20/2017.
  */
 public class ConfigHandler {
-    private static final List<AbstractConfig> ABSTRACT_CONFIGS;
+    private static final Map<Class<? extends AbstractConfig>, AbstractConfig<?>> CLASS_MAP;
+    private static final Map<String, AbstractConfig<?>>[] STRING_MAP;
     static {
-        ABSTRACT_CONFIGS = new ArrayList<>();
+        CLASS_MAP = new HashMap<>();
+        STRING_MAP = new Map[ConfigLevel.values().length];
+        for (int i = 0; i < STRING_MAP.length; i++) {
+            STRING_MAP[i] = new HashMap<>();
+        }
         EnumSet.allOf(ConfigLevel.class).forEach(configLevel -> {
             Reflections reflections = new Reflections("com.guthub.kaaz.discordbot.config.configs." + configLevel.name().replace("_", "").toLowerCase());
             Set<Class<? extends AbstractConfig>> classes = reflections.getSubTypesOf(AbstractConfig.class);
@@ -23,7 +28,8 @@ public class ConfigHandler {
                 try {
                     AbstractConfig config = clazz.newInstance();
                     config.setConfigLevel(configLevel);
-                    ABSTRACT_CONFIGS.add(config);
+                    CLASS_MAP.put(clazz, config);
+                    STRING_MAP[configLevel.ordinal()].put(config.getName(), config);
                 } catch (InstantiationException | IllegalAccessException e) {
                     Log.log("Exception during init of a config: " + clazz.getSimpleName(), e);
                 }
@@ -31,28 +37,17 @@ public class ConfigHandler {
         });
     }
     public static AbstractConfig getConfig(ConfigLevel level, String configName){
-        for (AbstractConfig config : ABSTRACT_CONFIGS) {
-            if (config.getConfigLevel() == level && config.getName().equals(configName)) {
-                return config;
-            }
-        }
-        return null;
+        return STRING_MAP[level.ordinal()].get(configName);
     }
     public static <E extends AbstractConfig> E getConfig(Class<E> clazz){
-        for (AbstractConfig config : ABSTRACT_CONFIGS) {
-            if (config.getClass().equals(clazz)) {
-                return (E) config;
-            }
+        Object e = CLASS_MAP.get(clazz);
+        if (e != null){
+            return (E) e;
         }
         throw new RuntimeException("Attempted searching for a non-existent config by using Class search: " + clazz.getClass().getName());
     }
-    public static <E extends AbstractConfig> void setSetting(Class<E> clazz, Configurable configurable, Object value){
-        E config = getConfig(clazz);
-        if (config != null){
-            config.setValue(configurable, value);
-        } else {
-            throw new RuntimeException("Attempted searching for a non-existent config by using Class search: " + clazz.getClass().getName());
-        }
+    public static <E extends AbstractConfig<F>, F> void setSetting(Class<E> clazz, Configurable configurable, F value){
+        getConfig(clazz).setValue(configurable, value);
     }
     public static boolean setSetting(String configName, Configurable configurable, Object value){
         AbstractConfig config = getConfig(configurable.getConfigLevel(), configName);
@@ -68,19 +63,10 @@ public class ConfigHandler {
         }
     }
     @SafeVarargs
-    public static <E extends AbstractConfig, F> F getSetting(Class<E> clazz, Configurable configurable, Holder<F>...holder){
-        E config = getConfig(clazz);
-        if (config != null){
-            try {
-                Object o = config.getValue(configurable);
-                Holder.fillOptional((F) o, holder);
-                return (F) o;
-            } catch (ClassCastException e){
-                throw new RuntimeException("Attempted to get a config value with the wrong type of holder", e);
-            }
-        } else {
-            throw new RuntimeException("Attempted searching for a non-existent config by using Class search: " + clazz.getClass().getName());
-        }
+    public static <E extends AbstractConfig<F>, F> F getSetting(Class<E> clazz, Configurable configurable, Holder<F>...holder){
+        Object o = getConfig(clazz).getValue(configurable);
+        Holder.fillOptional((F) o, holder);
+        return (F) o;
     }
     @SafeVarargs
     public static <E> E getSetting(String configName, Configurable configurable, Holder<E>...holder){
@@ -91,7 +77,7 @@ public class ConfigHandler {
                 Holder.fillOptional((E) o, holder);
                 return (E) o;
             } catch (ClassCastException e){
-                throw new RuntimeException("Attempted", e);
+                throw new RuntimeException("Attempted to get a value with the wrong holder type for the config: " + config.getName(), e);
             }
         } else {
             return null;
