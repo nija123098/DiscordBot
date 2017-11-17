862cb84f255cfc027b492512f0be966ed8ca49ce
Brandan Schmitz <Schmitzbrandan0@gmail.com>
2017-11-16 23:27:39 -0600

    Add current version checker to UpdateBotCommand

--- src/main/java/com/github/nija123098/evelyn/command/commands/UpdateBotCommand.java
+++ src/main/java/com/github/nija123098/evelyn/command/commands/UpdateBotCommand.java
@@ -1,25 +1,16 @@
 package com.github.nija123098.evelyn.command.commands;
 
 import com.github.nija123098.evelyn.command.AbstractCommand;
-import com.github.nija123098.evelyn.command.CommandHandler;
-import com.github.nija123098.evelyn.command.ContextType;
 import com.github.nija123098.evelyn.command.ModuleLevel;
-import com.github.nija123098.evelyn.command.annotations.Argument;
 import com.github.nija123098.evelyn.command.annotations.Command;
 import com.github.nija123098.evelyn.discordobjects.helpers.MessageMaker;
-import com.github.nija123098.evelyn.discordobjects.wrappers.Channel;
-import com.github.nija123098.evelyn.information.subsription.SubscriptionLevel;
 import com.github.nija123098.evelyn.launcher.Launcher;
 import com.github.nija123098.evelyn.service.services.ScheduleService;
-import com.github.nija123098.evelyn.template.KeyPhrase;
-import com.github.nija123098.evelyn.template.Template;
-import com.github.nija123098.evelyn.template.TemplateHandler;
 import com.github.nija123098.evelyn.util.ExecuteShellCommand;
 import com.github.nija123098.evelyn.util.HastebinUtil;
 import com.github.nija123098.evelyn.util.PlatformDetector;
 
 import java.io.IOException;
-import java.util.Collections;
 
 /**
  * @author Celestialdeath99
@@ -41,16 +32,22 @@ public class UpdateBotCommand extends AbstractCommand {
         } else if (PlatformDetector.isUnix()) {
             maker.append("The bot will now download, compile and update itself from the latest version on GitHub." + "\n");
             ExecuteShellCommand.commandToExecute("./Pull.sh");
-            maker.appendRaw("\n\n*GIT Pull Results:*\n```" + ExecuteShellCommand.getOutput() + "```");
-            ExecuteShellCommand.commandToExecute("./Build.sh");
-            if (ExecuteShellCommand.getOutput().contains("BUILD SUCCESS")) {
-                if (ExecuteShellCommand.getOutput().length() < 2000) {
-                    maker.appendRaw("\n*Compilation Results:*\n```" + ExecuteShellCommand.getOutput() + "```");
-                } else maker.appendRaw("\nCompilation Results:\n" + HastebinUtil.handleHastebin(ExecuteShellCommand.getOutput()));
-                maker.append("\n\n**The bot will now restart to apply the updates.**").send();
-                ScheduleService.schedule(5000, () -> Launcher.shutdown(1,0,false));
-                ExecuteShellCommand.commandToExecute("./Update.sh");
-            } else maker.appendRaw("\n**ERROR COMPILING BOT**. You can view the log here:\n" + HastebinUtil.handleHastebin(ExecuteShellCommand.getOutput()));
+            if (ExecuteShellCommand.getOutput().contains("Already up-to-date.")) {
+                maker.appendRaw("\n**The bot is already at the latest version. Aborting update sequence.**");
+            } else {
+                maker.appendRaw("\n\n*GIT Pull Results:*\n```" + ExecuteShellCommand.getOutput() + "```");
+                ExecuteShellCommand.commandToExecute("./Build.sh");
+                if (ExecuteShellCommand.getOutput().contains("BUILD SUCCESS")) {
+                    if (ExecuteShellCommand.getOutput().length() < 2000) {
+                        maker.appendRaw("\n*Compilation Results:*\n```" + ExecuteShellCommand.getOutput() + "```");
+                    } else
+                        maker.appendRaw("\nCompilation Results:\n" + HastebinUtil.handleHastebin(ExecuteShellCommand.getOutput()));
+                    maker.append("\n\n**The bot will now restart to apply the updates.**").send();
+                    ScheduleService.schedule(5000, () -> Launcher.shutdown(1, 0, false));
+                    ExecuteShellCommand.commandToExecute("./Update.sh");
+                } else
+                    maker.appendRaw("\n**ERROR COMPILING BOT**. You can view the log here:\n" + HastebinUtil.handleHastebin(ExecuteShellCommand.getOutput()));
+            }
         }
     }
 }
