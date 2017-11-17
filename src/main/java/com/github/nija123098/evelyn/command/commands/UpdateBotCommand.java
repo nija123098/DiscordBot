6fd0365f285cfd3839a22c01a42ed9874f061298
Brandan Schmitz <Schmitzbrandan0@gmail.com>
2017-11-16 20:25:23 -0600

    Revert ExecuteShellCommand to Runtime.getRuntime().exec()

--- src/main/java/com/github/nija123098/evelyn/command/commands/UpdateBotCommand.java
+++ src/main/java/com/github/nija123098/evelyn/command/commands/UpdateBotCommand.java
@@ -15,6 +15,7 @@ import com.github.nija123098.evelyn.template.KeyPhrase;
 import com.github.nija123098.evelyn.template.Template;
 import com.github.nija123098.evelyn.template.TemplateHandler;
 import com.github.nija123098.evelyn.util.ExecuteShellCommand;
+import com.github.nija123098.evelyn.util.HastebinUtil;
 import com.github.nija123098.evelyn.util.PlatformDetector;
 
 import java.io.IOException;
@@ -38,14 +39,14 @@ public class UpdateBotCommand extends AbstractCommand {
             osType = "macOS";
             maker.append("This command can only be run whe the bot is being hosted on a Linux server not " + osType + " which it is currently on");
         } else if (PlatformDetector.isUnix()) {
-            maker.append("The bot will now download, compile and update itself from the latest version on GitHub." + "\n" + "This usually takes 2-3 minutes.").send();
+            maker.append("The bot will now download, compile and update itself from the latest version on GitHub." + "\n" + "This usually takes 2-3 minutes. The output will be shown below.");
             ExecuteShellCommand.commandToExecute("./Pull.sh");
-            maker.clearMessage().append("The GIT pull complete with the following message:\n```").appendRaw(ExecuteShellCommand.getOutput()).append("```");
+            maker.appendRaw("\n```" + ExecuteShellCommand.getOutput() + "```\n");
             ScheduleService.schedule(17000, () -> ExecuteShellCommand.commandToExecute("./Build.sh"));
+            ScheduleService.schedule(78000, () -> maker.appendRaw(ExecuteShellCommand.getOutput().length() < 2000 ? ExecuteShellCommand.getOutput() : HastebinUtil.handleHastebin(ExecuteShellCommand.getOutput())));
             ScheduleService.schedule(80000, () -> ExecuteShellCommand.commandToExecute("./Update.sh"));
-            ScheduleService.schedule(83000, () -> Launcher.shutdown( 1, 0, false));
-
+            ScheduleService.schedule(81000, () -> maker.appendRaw("The compilation was successful. The bot will now restart."));
+            ScheduleService.schedule(82000, () -> Launcher.shutdown( 1, 0, false));
         }
-
     }
 }
--- src/main/java/com/github/nija123098/evelyn/util/ExecuteShellCommand.java
+++ src/main/java/com/github/nija123098/evelyn/util/ExecuteShellCommand.java
@@ -3,6 +3,7 @@ package com.github.nija123098.evelyn.util;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Celestialdeath99
@@ -10,18 +11,20 @@ import java.io.InputStreamReader;
 
 public class ExecuteShellCommand {
     static String output;
+
     public static void commandToExecute(String command) {
-        ProcessBuilder pb = new ProcessBuilder(command);
+        Process p;
         String line;
         try {
-            Process process = pb.start();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            p = Runtime.getRuntime().exec(command);
+            p.waitFor(1, TimeUnit.MINUTES);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
             StringBuilder builder = new StringBuilder();
             while ((line = reader.readLine()) !=null) {
                 builder.append(line).append("\n");
             }
             output = builder.toString();
-        } catch (IOException e) {
+        } catch (InterruptedException | IOException e) {
             e.printStackTrace();
         }
     }
