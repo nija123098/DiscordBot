94a9eb1a2ab72dfce48ab74ceb15a618616be08d
Dxeo <dxbabdulla@gmail.com>
2017-11-28 05:00:42 +0400

    make announce command look presentable

--- src/main/java/com/github/nija123098/evelyn/botmanagement/AnnounceCommand.java
+++ src/main/java/com/github/nija123098/evelyn/botmanagement/AnnounceCommand.java
@@ -9,7 +9,9 @@ import com.github.nija123098.evelyn.discordobjects.helpers.MessageMaker;
 import com.github.nija123098.evelyn.discordobjects.wrappers.Channel;
 import com.github.nija123098.evelyn.discordobjects.wrappers.DiscordClient;
 import com.github.nija123098.evelyn.moderation.logging.BotChannelConfig;
+import com.github.nija123098.evelyn.util.FormatHelper;
 
+import java.awt.*;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -22,6 +24,11 @@ public class AnnounceCommand extends AbstractCommand {
         MessageMaker maker = new MessageMaker((Channel) null);
         Set<Channel> channels = DiscordClient.getGuilds().stream().map((guild -> ConfigHandler.getSetting(BotChannelConfig.class, guild))).collect(Collectors.toSet());
         channels.remove(null);
+        maker.withAuthorIcon(DiscordClient.getOurUser().getAvatarURL());
+        maker.getAuthorName().appendRaw("Evelyn Announcement");
+        maker.withThumb("https://cdn.discordapp.com/attachments/374538747229896704/384862519354982403/38153-200.png");
+        maker.getFooter().appendRaw("Click " + FormatHelper.embedLink("here","https://discord.gg/UW5X5BU") + " to join the support server.");
+        maker.withColor(new Color(175, 30,5));
         for (Channel channel : channels) {
                 maker.forceCompile().withChannel(channel).appendRaw(text).send();
                 maker.forceCompile().clearMessage();
