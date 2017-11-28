588e2840e2d16953e23f43ef6d4e91c0f0025574
Dxeo <dxbabdulla@gmail.com>
2017-11-28 05:19:10 +0400

    change announce command to use local bot name for title

--- src/main/java/com/github/nija123098/evelyn/botmanagement/AnnounceCommand.java
+++ src/main/java/com/github/nija123098/evelyn/botmanagement/AnnounceCommand.java
@@ -25,13 +25,13 @@ public class AnnounceCommand extends AbstractCommand {
         Set<Channel> channels = DiscordClient.getGuilds().stream().map((guild -> ConfigHandler.getSetting(BotChannelConfig.class, guild))).collect(Collectors.toSet());
         channels.remove(null);
         maker.withAuthorIcon(DiscordClient.getOurUser().getAvatarURL());
-        maker.getAuthorName().appendRaw("Evelyn Announcement");
         maker.withThumb("https://cdn.discordapp.com/attachments/374538747229896704/384862519354982403/38153-200.png");
         maker.getFooter().appendRaw("Click " + FormatHelper.embedLink("here","https://discord.gg/UW5X5BU") + " to join the support server.");
         maker.withColor(new Color(175, 30,5));
         for (Channel channel : channels) {
-                maker.forceCompile().withChannel(channel).appendRaw(text).send();
-                maker.forceCompile().clearMessage();
+            maker.getAuthorName().appendRaw(DiscordClient.getOurUser().getDisplayName(channel.getGuild()) + " Announcement");
+            maker.forceCompile().withChannel(channel).appendRaw(text).send();
+            maker.forceCompile().clearMessage();
         }
     }
 }
