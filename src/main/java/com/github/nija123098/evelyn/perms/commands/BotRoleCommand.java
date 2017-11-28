56952a788ea9fd445796d1cdd508a9adc415aa50
Dxeo <dxbabdulla@gmail.com>
2017-11-28 20:10:06 +0400

    change botrole emotes to user the new green tick and red tick emotes

--- src/main/java/com/github/nija123098/evelyn/perms/commands/BotRoleCommand.java
+++ src/main/java/com/github/nija123098/evelyn/perms/commands/BotRoleCommand.java
@@ -2,14 +2,14 @@ package com.github.nija123098.evelyn.perms.commands;
 
 import com.github.nija123098.evelyn.command.AbstractCommand;
 import com.github.nija123098.evelyn.command.ModuleLevel;
-import com.github.nija123098.evelyn.command.annotations.Command;
 import com.github.nija123098.evelyn.command.annotations.Argument;
+import com.github.nija123098.evelyn.command.annotations.Command;
 import com.github.nija123098.evelyn.discordobjects.helpers.MessageMaker;
 import com.github.nija123098.evelyn.discordobjects.wrappers.Guild;
 import com.github.nija123098.evelyn.discordobjects.wrappers.User;
 import com.github.nija123098.evelyn.perms.BotRole;
-import com.github.nija123098.evelyn.util.EmoticonHelper;
 import com.github.nija123098.evelyn.util.FormatHelper;
+import com.github.nija123098.evelyn.util.GeneralEmotes;
 
 import java.util.Set;
 import java.util.stream.Stream;
@@ -26,6 +26,7 @@ public class BotRoleCommand extends AbstractCommand {
         maker.getAuthorName().appendAlternate(true, FormatHelper.makePleural(user.getDisplayName(guild)) + " BotRoles");
         maker.withAuthorIcon(user.getAvatarURL());
         Set<BotRole> botRoles = BotRole.getSet(user, guild);
-        Stream.of(BotRole.values()).forEach(role -> maker.getNewFieldPart().getTitle().appendRaw(role.name()).getFieldPart().getValue().appendRaw(EmoticonHelper.getChars(botRoles.contains(role) ? "white_check_mark" : "x", false)));
+        maker.getHeader().clear().appendRaw("\u200B\n");
+        Stream.of(BotRole.values()).forEach(role -> maker.getNewFieldPart().getTitle().appendRaw(role.name()).getFieldPart().getValue().appendRaw(botRoles.contains(role) ? GeneralEmotes.GREENTICK : GeneralEmotes.REDTICK));
     }
 }
