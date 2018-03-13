bbd594a34949c97cb57e89b6e0b2e8f6a5342117
Brandan Schmitz <Schmitzbrandan0@gmail.com>
2018-03-12 23:34:56 -0500

    Correct NPE for null file path

--- src/main/java/com/github/nija123098/evelyn/util/ExecuteShellCommand.java
+++ src/main/java/com/github/nija123098/evelyn/util/ExecuteShellCommand.java
@@ -1,5 +1,7 @@
 package com.github.nija123098.evelyn.util;
 
+import com.github.nija123098.evelyn.botconfiguration.ConfigProvider;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -16,6 +18,7 @@ public class ExecuteShellCommand {
     public static void commandToExecute(String command, String filePath) {
         Runtime rt = Runtime.getRuntime();
         Process p;
+        if (filePath==null) { filePath = ConfigProvider.BOT_SETTINGS.botFolder(); }
         String line;
         try {
             p = rt.exec(command, null, new File(filePath));
