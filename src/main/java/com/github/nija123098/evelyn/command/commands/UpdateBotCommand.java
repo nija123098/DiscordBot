cedd957d550f0f420adf146b9295c0f933ecb0d2
Brandan Schmitz <Schmitzbrandan0@gmail.com>
2018-03-12 20:24:21 -0500

    Fix message sending in UpdateBotCommand

--- src/main/java/com/github/nija123098/evelyn/command/commands/UpdateBotCommand.java
+++ src/main/java/com/github/nija123098/evelyn/command/commands/UpdateBotCommand.java
@@ -28,20 +28,21 @@ public class UpdateBotCommand extends AbstractCommand {
             maker.append("Please check the settings in the config files. The currently set directory is not a valid git directory.").send();
         } else {
             if (ExecuteShellCommand.getOutput().contains("Already up-to-date")) {
-                maker.append("Your local repository is already up to date.");
+                maker.append("Your local repository is already up to date.").send();
             }
-            maker.append("Please wait while the update is compiled.");
+            maker.append("Please wait while the update is compiled.").send();
             ExecuteShellCommand.commandToExecute("cd " + ConfigProvider.UPDATE_SETTINGS.updateFolder() + " && mvn " + ConfigProvider.UPDATE_SETTINGS.mvnArgs());
             if (ExecuteShellCommand.getOutput().contains("BUILD SUCCESS")) {
-                maker.appendRaw("The project was compiled successfully. Now starting the new bot.");
+                maker.appendRaw("The project was compiled successfully. Now starting the new bot.").send();
             } else {
-                maker.appendRaw("**The update was unsuccessful. Please view the build results here:**" + PastebinUtil.postToPastebin("Maven Compile Log", ExecuteShellCommand.getOutput()));
+                maker.appendRaw("**The update was unsuccessful. Please view the build results here:**" + PastebinUtil.postToPastebin("Maven Compile Log", ExecuteShellCommand.getOutput())).send();
             }
             if (PlatformDetector.isUnix() || PlatformDetector.isWindows()) {
                 ExecuteShellCommand.commandToExecute("cp " + ConfigProvider.UPDATE_SETTINGS.updateFolder() + "target/DiscordBot-1.0.0.jar " + ConfigProvider.BOT_SETTINGS.botFolder() + "Evelyn.jar");
                 ExecuteShellCommand.commandToExecute(ConfigProvider.BOT_SETTINGS.startCommand());
+                maker.append("The updated version of the bot has beed started. Please allow 1-2 minutes for changes to take effect.").send();
             } else if (PlatformDetector.isMac()) {
-                maker.append("I am sorry, I do not know the commands needed to make this work for macOS computers. Please manually update the bot.");
+                maker.append("I am sorry, I do not know the commands needed to make this work for macOS computers. Please manually update the bot.").send();
             }
         }
     }
