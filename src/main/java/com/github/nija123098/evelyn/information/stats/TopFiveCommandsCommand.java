c50c78ca2429aafac51abe004aa37b67b5745273
Soarnir <soarnir@gmail.com>
2018-04-20 13:29:49 +0200

    fix topfive command sorting

--- src/main/java/com/github/nija123098/evelyn/information/stats/TopFiveCommandsCommand.java
+++ src/main/java/com/github/nija123098/evelyn/information/stats/TopFiveCommandsCommand.java
@@ -7,10 +7,9 @@ import com.github.nija123098.evelyn.discordobjects.helpers.MessageMaker;
 import com.github.nija123098.evelyn.discordobjects.wrappers.DiscordClient;
 import com.github.nija123098.evelyn.discordobjects.wrappers.User;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 /**
  * @author Soarnir
@@ -24,8 +23,9 @@ public class TopFiveCommandsCommand extends AbstractCommand {
 
     @Command
     public void command(MessageMaker maker) {
-        Map<User, Integer> userCommandCount = getStats();
-        List<Map.Entry<User, Integer>> list = new LinkedList<>(userCommandCount.entrySet());
+        Map<User, Integer> sortedMap = getStats().entrySet().stream().sorted(Map.Entry.<User, Integer>comparingByValue().reversed())
+                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        List<Map.Entry<User, Integer>> list = new LinkedList<>(sortedMap.entrySet());
         maker.mustEmbed();
         maker.getTitle().appendRaw("Top 5 Users: Commands");
         for (int i = 0; i < 5; i++) {
@@ -35,7 +35,7 @@ public class TopFiveCommandsCommand extends AbstractCommand {
 
     private Map<User, Integer> getStats() {
         return Database.select("SELECT * FROM commands_used_count_user ORDER BY VALUE DESC", set -> {
-            Map<User, Integer> temp = new ConcurrentHashMap<>();
+            Map<User, Integer> temp = new HashMap<>();
             for (int i = 1; i < 6; i++) {
                 set.next();
                 temp.put((User.getUser(set.getString(1)) != null ? (User.getUser(set.getString(1))) : DiscordClient.getOurUser()), set.getInt(2));
