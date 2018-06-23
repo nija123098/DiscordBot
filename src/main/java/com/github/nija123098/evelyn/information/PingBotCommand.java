ed6304457831c01ebff7f57930ed3a8b0f873863
Unknown <Schmitzbrandan0@gmail.com>
2018-06-23 14:13:14 -0400

    Add version of ping command for management-bot

---
+++ src/main/java/com/github/nija123098/evelyn/information/PingBotCommand.java
@@ -0,0 +1,27 @@
+package com.github.nija123098.evelyn.information;
+
+import com.github.nija123098.evelyn.command.AbstractCommand;
+import com.github.nija123098.evelyn.command.annotations.Command;
+import com.github.nija123098.evelyn.discordobjects.helpers.MessageMaker;
+import com.github.nija123098.evelyn.discordobjects.wrappers.Message;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+/**
+ * @author Celestialdeath99
+ * @since 1.0.0
+ */
+public class PingBotCommand extends AbstractCommand {
+
+    public PingBotCommand() {
+        super(PingCommand.class, "bot", null, null, null, "A plain version of the ping command for bots.");
+    }
+
+    @Command
+    public void command(MessageMaker maker, Message message) {
+        Instant sysTime = Instant.now();
+        Instant messageTime = message.getCreationDate();
+        long timeDiff = ChronoUnit.MILLIS.between(messageTime, sysTime);
+        maker.appendRaw(String.valueOf(timeDiff));
+    }
+}
