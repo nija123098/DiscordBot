496cac7e6898ca1c0fcca0a8b1eb1c6fe2941a11
Jack S <nija123098@gmail.com>
2018-07-23 21:50:13 -0500

    Update DB dependencies and restart periodically

--- src/main/java/com/github/nija123098/evelyn/config/Database.java
+++ src/main/java/com/github/nija123098/evelyn/config/Database.java
@@ -4,6 +4,7 @@ import com.github.nija123098.evelyn.botconfiguration.ConfigProvider;
 import com.github.nija123098.evelyn.exception.DevelopmentException;
 import com.github.nija123098.evelyn.util.CallBuffer;
 import com.github.nija123098.evelyn.util.Log;
+import com.github.nija123098.evelyn.util.ThreadHelper;
 import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
@@ -12,6 +13,10 @@ import org.apache.commons.dbutils.ResultSetHandler;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Controls interaction with the database.
@@ -22,9 +27,18 @@ import java.sql.SQLException;
  */
 public class Database {
     private static final CallBuffer CALL_BUFFER = new CallBuffer("Database", 250);
-    private static final Connection CONNECTION;
-    private static final QueryRunner RUNNER;
-    static {
+    private static final AtomicReference<Connection> CONNECTION = new AtomicReference<>();
+    private static final AtomicReference<QueryRunner> RUNNER = new AtomicReference<>();
+    private static final AtomicReference<HikariDataSource> DATA_SOURCE = new AtomicReference<>();
+    private static final AtomicBoolean INITIALIZED = new AtomicBoolean();
+    private static final Object INITIALIZE_LOCK = new Object();
+
+    public static void init() {
+        Executors.newSingleThreadScheduledExecutor(r -> ThreadHelper.getDemonThreadSingle(r, "DB-Restart-Executor")).scheduleAtFixedRate(Database::initializeDB, 0, 4, TimeUnit.HOURS);
+    }
+
+    private static void initializeDB() {
+        INITIALIZED.set(false);
         String username = ConfigProvider.DATABASE_SETTINGS.username();
         String password = ConfigProvider.DATABASE_SETTINGS.password();
         String serverIP = ConfigProvider.DATABASE_SETTINGS.ipAddress();
@@ -46,18 +60,41 @@ public class Database {
         } catch (SQLException e) {
             Log.log("Could not init database connection", e);
         }
-        CONNECTION = c;
+        Connection oldConnection = CONNECTION.getAndSet(c);
         HikariConfig config = new HikariConfig();
         config.setMaximumPoolSize(20);
         config.setJdbcUrl("jdbc:mariadb://" + serverIP + ":" + serverPort + "/" + dbName + "?characterEncoding=utf8mb4");
         config.setDriverClassName("org.mariadb.jdbc.Driver");
         config.setUsername(username);
         config.setPassword(password);
-        RUNNER = new QueryRunner(new HikariDataSource(config));
+        HikariDataSource source = new HikariDataSource(config);
+        HikariDataSource oldSource = DATA_SOURCE.getAndSet(source);
+        RUNNER.set(new QueryRunner(source));
+        INITIALIZED.set(true);
         query("SET NAMES utf8mb4");
+        synchronized (INITIALIZE_LOCK) {
+            INITIALIZE_LOCK.notifyAll();
+        }
+        if (oldConnection != null) {
+            try {
+                oldSource.close();
+                oldConnection.close();
+            } catch (SQLException e) {
+                Log.log("Exception closing connection to DB", e);
+            }
+        }
     }
 
-    public static void init() {
+    public static void checkInitialization() {
+        if (!INITIALIZED.get()) {
+            try {
+                synchronized (INITIALIZE_LOCK) {
+                    INITIALIZE_LOCK.wait();
+                }
+            } catch (InterruptedException e) {
+                Log.log("Unexpected interrupt while waiting for init lock on DB");
+            }
+        }
     }
 
     public static void bufferCall(Runnable runnable) {
@@ -65,25 +102,29 @@ public class Database {
     }
 
     public static Connection getConnection() {
-        return CONNECTION;
+        checkInitialization();
+        return CONNECTION.get();
     }
 
     public static <E> E select(String sql, ResultSetHandler<E> handler) {
-        try{return RUNNER.query(sql, handler);
+        checkInitialization();
+        try{return RUNNER.get().query(sql, handler);
         } catch (SQLException e) {
             throw new DevelopmentException("Could not select: ERROR: " + e.getErrorCode() + " " + sql, e);
         }
     }
 
     public static void query(String sql) {
-        try{RUNNER.update(sql);
+        checkInitialization();
+        try{RUNNER.get().update(sql);
         } catch (SQLException e) {
             throw new DevelopmentException("Could not query: ERROR: " + e.getErrorCode() + " " + sql, e);
         }
     }
 
     public static void insert(String sql) {
-        try{RUNNER.update(sql);
+        checkInitialization();
+        try{RUNNER.get().update(sql);
         } catch (SQLException e) {
             throw new DevelopmentException("Could not insert: ERROR: " + e.getErrorCode() + " " + sql, e);
         }
