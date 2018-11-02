354a4c4803440da3000debf055a2e26009d65210
Soarnir <eadb13b1@opayq.com>
2018-11-02 02:09:00 +0000

    upgrade archive command

--- src/main/java/com/github/nija123098/evelyn/moderation/ArchiveCommand.java
+++ src/main/java/com/github/nija123098/evelyn/moderation/ArchiveCommand.java
@@ -7,6 +7,7 @@ import com.github.nija123098.evelyn.command.annotations.Argument;
 import com.github.nija123098.evelyn.command.annotations.Command;
 import com.github.nija123098.evelyn.discordobjects.helpers.MessageMaker;
 import com.github.nija123098.evelyn.discordobjects.wrappers.*;
+import com.github.nija123098.evelyn.exception.ArgumentException;
 import com.github.nija123098.evelyn.exception.ContextException;
 import com.github.nija123098.evelyn.exception.DevelopmentException;
 import com.github.nija123098.evelyn.exception.PermissionsException;
@@ -20,6 +21,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.Charset;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
@@ -34,11 +37,12 @@ public class ArchiveCommand extends AbstractCommand {
     private static final EnumSet<DiscordPermission> USER_PERMISSIONS_REQUIRED = EnumSet.of(DiscordPermission.READ_MESSAGES, DiscordPermission.READ_MESSAGE_HISTORY);
 
     public ArchiveCommand() {
-        super("archive", BotRole.USER, ModuleLevel.ADMINISTRATIVE, null, null, "Archives all data to a txt file");
+        super("archive", BotRole.USER, ModuleLevel.ADMINISTRATIVE, null, null, "Archives all channel messages to a txt file");
     }
 
     @Command
-    public void command(@Argument(optional = true, replacement = ContextType.NONE) Time time, Channel channel, @Argument(optional = true, info = "timezone") ZoneId zoneId, String linux, MessageMaker maker, User user) {
+    public void command(@Argument(optional = true, replacement = ContextType.NONE) Time time, Channel channel, Guild guild, @Argument(optional = true, info = "timezone") ZoneId zoneId, @Argument(optional = true, info = "send dm", replacement = ContextType.NONE) Boolean sendDm, @Argument(optional = true, info = "detailed") String string, String linux, MessageMaker maker, User user) {
+        boolean detailed = false;
         if (channel.isPrivate()) throw new ContextException("You must use this command in a server");
         if (!channel.getModifiedPermissions(user).containsAll(USER_PERMISSIONS_REQUIRED)) {
             throw new PermissionsException("You do not have read access to that channel");
@@ -46,21 +50,39 @@ public class ArchiveCommand extends AbstractCommand {
         List<Message> messages = time == null ? channel.getMessages() : channel.getMessagesTo(System.currentTimeMillis() - time.timeUntil());
         Collections.reverse(messages);
         Charset charset = Charset.forName("UTF-8");
-        File file = FileHelper.getTempFile("archive", "txt");// may want to put to hastebin
+
+        //create file
+        String date = LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        File file = FileHelper.getTempFile("archive","txt",channel.getName() + "-" + guild.getName() + "-" + date);
+
         boolean nEnding = linux.equalsIgnoreCase("linux") || linux.equalsIgnoreCase("unix");
         String ending = nEnding ? "\n" : "\r\n";
         try {
+            if (!string.isEmpty()) {
+                if (string.equalsIgnoreCase("detailed")) {
+                    detailed = true;
+                } else {
+                    throw new ArgumentException("type out `detailed` to create a detailed report with message ids");
+                }
+            }
             OutputStream outputStream = new FileOutputStream(file, true);
-            outputStream.write(("ARCHIVE for channel " + channel.getName() + " ID: " + channel.getID()).getBytes(charset));
+            outputStream.write(("Archive for channel #" + channel.getName() + " ID: " + channel.getID()).getBytes(charset));
+            outputStream.write(("\nGuild: " + guild.getName() +  " ID: " + guild.getID()).getBytes(charset));
+            outputStream.write(("\nReport taken: " + Instant.now().atZone(zoneId).format(DateTimeFormatter.ISO_DATE_TIME) + "\n").getBytes(charset));
+            boolean finalDetailed = detailed;
             messages.forEach(message -> {// write messages to file
                 StringBuilder builder = new StringBuilder(message.getContent().length() + 200);
-                builder.append(ending).append("ID: ").append(message.getID())
-                        .append(" TIME: ").append(message.getCreationDate().atZone(zoneId).format(DateTimeFormatter.ISO_DATE_TIME))
-                        .append(" COM_TIME: ").append(message.getCreationDate().toEpochMilli())
-                        .append(" AUTHOR: ").append(message.getAuthor().getNameAndDiscrim())
-                        .append(" AUTHOR_ID: ").append(message.getAuthor().getID());
-                message.getAttachments().forEach(attachment -> builder.append(" ATTACHMENT: ").append(attachment.getUrl()));
-                builder.append(" MESSAGE:").append(ending).append(nEnding ? message.getContent() : message.getContent().replace("\n", ending));
+                if (finalDetailed) {
+                    builder.append(ending).append(message.getAuthor().getNameAndDiscrim())
+                            .append(" user_id: ").append(message.getID())
+                            .append("\ntime: ").append(message.getCreationDate().atZone(zoneId).format(DateTimeFormatter.ISO_DATE_TIME))
+                            .append(" message_id: ").append(message.getAuthor().getID());
+                } else {
+                    builder.append(ending).append(message.getAuthor().getNameAndDiscrim())
+                            .append(" at: ").append(message.getCreationDate().atZone(zoneId).format(DateTimeFormatter.ISO_DATE_TIME));
+                }
+                message.getAttachments().forEach(attachment -> builder.append("\nATTACHMENT: ").append(attachment.getUrl()));
+                builder.append("\nMESSAGE:").append(ending).append(nEnding ? message.getContent() : message.getContent().replace("\n", ending)).append("\n");
                 try {
                     outputStream.write(builder.toString().getBytes(charset));
                 } catch (IOException e) {
@@ -70,6 +92,11 @@ public class ArchiveCommand extends AbstractCommand {
         } catch (IOException e) {
             Log.log("Unable to write to file", e);
         }
-        maker.withFile(file);
+        if (sendDm) {
+            new MessageMaker(user.getOrCreatePMChannel()).shouldEmbed(false).withFile(file).send();
+        } else {
+            maker.withFile(file).send();
+        }
     }
 }
+
